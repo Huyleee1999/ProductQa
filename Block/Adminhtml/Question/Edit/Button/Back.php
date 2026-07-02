fe49fb82af023564a76ad4a1293071642868bcaf
Lê Hoàng Huy <121824602+Huyleee1999@users.noreply.github.com>
2026-07-02 09:37:17 +0700

    fix button back in week 5

--- Block/Adminhtml/Question/Edit/Button/Back.php
+++ Block/Adminhtml/Question/Edit/Button/Back.php
@@ -2,15 +2,27 @@
 
 namespace Training\ProductQa\Block\Adminhtml\Question\Edit\Button;
 
+use Magento\Backend\Model\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
 class Back implements ButtonProviderInterface
 {
+    private UrlInterface $urlBuilder;
+
+    public function __construct(
+        UrlInterface $urlBuilder
+    ) {
+        $this->urlBuilder = $urlBuilder;
+    }
+
     public function getButtonData()
     {
         return [
             'label' => __('Back'),
-            'on_click' => "location.href = '.../question/index'",
+            'on_click' => sprintf(
+                "location.href = '%s';",
+                $this->urlBuilder->getUrl('productqa/question/index')
+            ),
             'class' => 'back',
             'sort_order' => 20
         ];
